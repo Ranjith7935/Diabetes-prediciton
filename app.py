d1d1099457928e468b39bd583c7f6b16803eb523
Ranjith7935 <rkranjithkumar009@gmail.com>
2024-09-14 16:30:40 +0530

    Last commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import pickle
 from flask  import Flask,request,app,jsonify,url_for,render_template
 import numpy as np
@@ -20,13 +21,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
-@app.route('/predict',method=["POST"])
+@app.route('/predict',methods=["POST"])
 def predict():
     data=[float(x)/100 for x in request.form.values()]
     final_input=scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
-    output=output.predict(final_input[0])
-    return render_template('home.html',prediction_text="The value for the diabetes is ()".format(output))
+    output=regmodel.predict(final_input)[0]
+    return render_template('home.html',prediction_text="The predicted value for the diabetes is {}".format(output))
 
 if __name__=="__main__":
     app.run(debug=True)
